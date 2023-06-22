fc45435f559ada43c3e75114863e8b24cf107e5e
Guidotss <guidoolguin@hotmail.com>
2023-06-22 19:11:52 -0300

    Terminando version mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,40 @@ import { SearchIp } from "@/components";
 import { IpData, Map } from "@/components/ui";
 import Image from "next/image";
 
-
 export default function Home() {
   return (
-    <main className="">
+    <main className="overflow-hidden">
       <header className="w-full">
         <Image
-          className="w-full object-contain"
+          className="w-full object-contain hidden sm:block"
           src="/images/pattern-bg-desktop.png"
           alt="Header Image"
           width={1440}
           height={400}
         />
+
+        <Image
+          className="w-full object-contain sm:hidden"
+          src="/images/pattern-bg-mobile.png"
+          alt="Header Image"
+          width={375}
+          height={400}
+        />
         <div className="absolute left-[550px] 2xl:left-[750px] 2xl:top-14 top-5">
-          <h1 className="text-slate-50 text-2xl 2xl:text-4xl font-semibold tracking-wider">IP Address Tracker</h1>
+          <h1 className="text-slate-50 text-2xl 2xl:text-4xl font-semibold tracking-wider">
+            IP Address Tracker
+          </h1>
         </div>
-        <div className="absolute 2xl:top-40 2xl:left-[660px] top-28 left-[450px] ">
-          <SearchIp/>
+        <div className="absolute 2xl:top-40 2xl:left-[660px] top-16 left-11  ">
+          <SearchIp />
         </div>
       </header>
-        <div className="flex absolute top-48 left-[190px] 2xl:top-72 2xl:left-[310px] z-10 shadow-xl">  
-          <IpData/>
-        </div>
-        <div className="w-full absolute">
-          <Map/>
-        </div>
+      <div className="absolute top-48 sm:left-[190px] 2xl:top-72 2xl:left-[310px] z-10 shadow-xl left-16">
+        <IpData />
+      </div>
+      <div className="w-full absolute">
+        <Map />
+      </div>
     </main>
-  )
+  );
 }
--- src/components/ui/IpData.tsx
+++ src/components/ui/IpData.tsx
@@ -6,21 +6,21 @@ export const IpData = () => {
   console.log(ipLocation);
 
   return (
-    <div className="flex bg-slate-50 shadow-lg 2xl:w-[1200px] h-40 rounded-lg p-8 z-10">
-      <div className="border-r border-gray-300 w-full items-center font-bold">
-        <h4 className="text-gray-500 text-sm mb-4">Ip Address</h4>
+    <div className="flex flex-col justify-center sm:flex sm:flex-row  bg-slate-50 shadow-lg 2xl:w-[1200px] h-56 rounded-lg sm:p-10 z-10 w-[240px]">
+      <div className="sm:border-r sm:border-gray-300 w-full font-bold flex flex-col items-center justify-center sm:block">
+        <h4 className="text-gray-500 text-sm sm:mb-4">Ip Address</h4>
         <span className="text-xl">{ipLocation?.ip}</span>
       </div>
-      <div className="border-r border-gray-300 w-full font-bold text-sm ml-4 items-center">
+      <div className="sm:border-r sm:border-gray-300 w-full font-bold text-sm sm:ml-4 flex flex-col items-center justify-center sm:block">
         <h4 className="text-gray-500 mb-2 text-sm">Location</h4>
         <span className="text-xl">{ipLocation?.city}</span>
       </div>
-      <div className="border-r border-gray-300 w-full font-bold items-center ml-4 text-start">
-        <h4 className="text-sm text-gray-500 mb-4">Timezone</h4>
-        <span className="text-xl">{ipLocation?.timezone}</span>
+      <div className="sm:border-r sm:border-gray-300 w-full font-bold sm:ml-4 sm:text-start flex flex-col items-center justify-center sm:block">
+        <h4 className="text-sm text-gray-500 sm:mb-4">Timezone</h4>
+        <span className="text-xl ">{ipLocation?.timezone}</span>
       </div>
-      <div className="w-full font-bold items-center ml-4">
-        <h4 className="text-sm text-gray-500 mb-3">ISP</h4>
+      <div className="w-full font-bold sm:ml-4 flex flex-col items-center justify-center sm:block">
+        <h4 className="text-sm text-gray-500 sm:mb-3">ISP</h4>
         <span className="text-xl">{ipLocation?.isp}</span>
       </div>
       
